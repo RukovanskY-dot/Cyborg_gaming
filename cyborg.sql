e8ff105da33a6cc6cfaabfdd7280dae4fa974376
RukovanskY-dot <rukoarmin@gmail.com>
2025-06-19 08:32:42 +0200

    DB

--- cyborg.sql
+++ cyborg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hostiteľ: 127.0.0.1
--- Čas generovania: Po 16.Jún 2025, 14:54
+-- Čas generovania: Št 19.Jún 2025, 08:31
 -- Verzia serveru: 10.4.32-MariaDB
 -- Verzia PHP: 8.2.12
 
@@ -32,20 +32,18 @@ CREATE TABLE `hráči` (
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `Email` varchar(70) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Sťahujem dáta pre tabuľku `hráči`
 --
 
-INSERT INTO `hráči` (`id`, `first_name`, `last_name`, `Email`, `password`) VALUES
-(1, '', '', '', ''),
-(2, 'Ármin', 'Rukovánsky', 'alma@gmail.com', ''),
-(3, 'jani', 'Rukovánsky', 'ala@gmail.com', ''),
-(4, 'Monika', 'Rukovansky', 'rukoarmin@gmail.com', ''),
-(5, 'Jancsi', 'Alma', 'eghrgh@fdf.com', '$2y$10$Y5rkWjppWRW8./5ncmP9eeh4T7QEqhk0cFLViRU7I5fYAquhLTp3K'),
-(6, 'luki', 'velky', 'abhzs@gs.com', '$2y$10$SNu0xTzSkouNrAmI7RgqjeX/LgKiqGirshDdd8Ly13iZ7ij.5PYNq');
+INSERT INTO `hráči` (`id`, `first_name`, `last_name`, `Email`, `password`, `role`) VALUES
+(16, 'alma', 'jancsi', 'asd@asd.com', '$2y$10$H997HNudPOL4Aunp01KO2.gol6TIEbnynUnHMjnBkUWFE.dUn5bje', 'admin'),
+(18, 'luki', 'nagy', 'em@em.com', '$2y$10$JCAQuchCj7ZQ5xyNLAaoWe152GDLIvS5Zme/Hf0wNrn9im/lXwAQ2', 'user'),
+(20, 'zoltan', 'Miklos', 'zoli@zoli.com', '$2y$10$Ca02Qu0LWFBRu7lmqoElSe4fB8EgUdF3XpU9JwH4tEelyaq3zTsva', 'admin');
 
 --
 -- Kľúče pre exportované tabuľky
@@ -65,7 +63,7 @@ ALTER TABLE `hráči`
 -- AUTO_INCREMENT pre tabuľku `hráči`
 --
 ALTER TABLE `hráči`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
